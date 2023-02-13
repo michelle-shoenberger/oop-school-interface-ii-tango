d80dfbe24d3973e02d7cce7192b0dce099355bfc
mickey <shoey25ms@gmail.com>
2023-02-13 18:04:08 -0500

    My Answer

--- classes/school.py
+++ classes/school.py
@@ -6,3 +6,12 @@ class School:
         self.name = name
         self.staff = Staff.objects()
         self.students = Student.objects()
+
+    def list_students(self):
+        for i, student in enumerate(self.students):
+            print(f"{i+1}. {student.name} {student.school_id}")
+
+    def find_student_by_id(self, id):
+        for student in self.students:
+            if student.school_id == id:
+                return student
